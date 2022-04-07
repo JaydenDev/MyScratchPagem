bf5a924bae290330a1cc1085fd2facb6eac7a9d9
JaydenDev <willsjayden2@gmail.com>
2022-04-06 20:57:47 -0500

    url args test 1

--- global.js
+++ global.js
@@ -59,3 +59,10 @@ function fetchData() {
     const img = "https://my-ocular.jeffalo.net/api/user/" + input + "/picture";
     $('#pfp').attr('src', img);
 }
+
+// if url argument "user" is not empty, parse it
+if (window.location.href.split('?')[1] !== undefined) {
+    const user = window.location.href.split('?')[1].split('=')[1];
+    document.getElementById('input').value = user;
+    fetchData();
+}
