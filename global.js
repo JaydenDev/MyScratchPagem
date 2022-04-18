9822f163d22e6c3fc6a48a6e1641a068a2e883e4
JaydenDev <willsjayden2@gmail.com>
2022-04-18 17:14:26 -0500

    test

--- global.js
+++ global.js
@@ -141,3 +141,10 @@ function refreshStat() {
     // for debug
     document.getElementById("signature").innerText = sig;
 }
+
+// if url arguments contains user, run fetchData on the user
+if (window.location.href.split('?')[1] !== undefined) {
+    const user = window.location.href.split('?')[1].split('=')[1];
+    document.getElementById('input').value = user;
+    fetchData();
+}
